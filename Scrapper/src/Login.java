838283956df640671eb6a77dfb7a9cdfb321f756
Taha Tariq <tahatariqf@gmail.com>
2026-05-11 03:48:16 -0700

    Login form

--- Scrapper/src/Login.java
+++ Scrapper/src/Login.java
@@ -1,6 +1,7 @@
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
@@ -16,8 +17,12 @@ import javafx.scene.image.Image;
 
 import static javafx.application.Application.launch;
 
-public class Launcher {
-    public static Scene startScene(Stage stage) {
+public class Login extends Application {
+    @Override
+    public void start(Stage stage) {
+
+        stage.setTitle("Login page");
+        stage.setResizable(false);
 
 
         GridPane layout = new GridPane();
@@ -26,76 +31,81 @@ public class Launcher {
         layout.setVgap(10);
 
 
-        Label scrapper = new Label("Scrapper");
+        Label scrapper = new Label("Welcome to webscraper!");
         scrapper.setTextFill(Color.color(0.4,0,0));
         scrapper.setFont(Font.font("Arial", FontWeight.BOLD,34));
 //        stage.getIcons().add(new Image(getClass().getResourceAsStream("/download.jpg")));
 
 
-        Label WebUrl = new Label("Website Url:");
-        WebUrl.setTextFill(Color.color(0.4,0,0));
-        WebUrl.setFont(Font.font("Arial",FontWeight.BOLD,20));
-        Label FileLoc = new Label("File location:");
-        FileLoc.setTextFill(Color.color(0.4,0,0));
-        FileLoc.setFont(Font.font("Arial",FontWeight.BOLD,20));
+        Label Email = new Label("Enter Email: ");
+        Email.setTextFill(Color.color(0.4,0,0));
+        Email.setFont(Font.font("Arial",FontWeight.BOLD,20));
+        Label passwordLabel = new Label("Enter Password: ");
+        passwordLabel.setTextFill(Color.color(0.4,0,0));
+        passwordLabel.setFont(Font.font("Arial", FontWeight.BOLD, 20));
 
 
         TextField web=new TextField();
         web.setFont(Font.font(14));
-        TextField file=new TextField();
+        PasswordField file=new PasswordField();
         file.setFont(Font.font(14));
 
 
         Label message=new Label();
-        Button extract = new Button("Extract");
+        Button login = new Button("Login");
         Button cancel = new Button("Cancel");
-        extract.setOnAction(e -> {
+        Button forgotPassword = new Button("Forgot Password?");
+        login.setOnAction(e -> {
 
-            String link = web.getText();
-            String filepath = file.getText();
-
-            Scraper scraper = new Scraper(link, filepath);
-
-            scraper.start_scrape();
+            Scene launcherScene = Launcher.startScene(stage);
+            stage.setScene(launcherScene);
 
             SetControl(
                     message,
                     "Data scraped successfully and File written successfully.",
                     stage,
-                    extract,
-                    cancel
+                    login,
+                    cancel,
+                    forgotPassword
             );
-        });        cancel.setOnAction(e-> SetControl(message,"Program exited successfully",stage,extract,cancel));
-        extract.setFont(Font.font(14));
+        });
+        cancel.setOnAction(e-> SetControl(message,"Program exited successfully",stage,login,cancel,forgotPassword));
+        login.setFont(Font.font(14));
         cancel.setFont(Font.font(14));
+        forgotPassword.setFont(Font.font(14));
 
 
         HBox Buttonbox = new HBox();
-        Buttonbox.getChildren().addAll(extract, cancel);
+        Buttonbox.getChildren().addAll(login, cancel,forgotPassword);
 
 
         layout.add(scrapper, 1,0);
-        layout.add(WebUrl, 0, 1);
-        layout.add(FileLoc, 0, 2);
+        layout.add(Email, 0, 1);
+        layout.add(passwordLabel, 0, 2);
+//        layout.add(FileLoc, 0, 2);
         layout.add(Buttonbox, 1, 3);
         layout.add(message,1,4);
         layout.setStyle("-fx-background-color: tan;");
         layout.add(web,1,1);
         layout.add(file,1,2);
 
-        return new Scene(layout,500,300);
+
+        Scene scene1 = new Scene(layout, 800, 400);
+        stage.setScene(scene1);
+        stage.show();
     }
 
     public static void main(String[] args) {
         launch();
     }
 
-    public static void SetControl(Label label,String text,Stage stage,Button extract,Button cancel){
+    public static void SetControl(Label label,String text,Stage stage,Button extract,Button cancel, Button forgot){
         label.setText(text);
         label.setFont(Font.font("Arial",FontWeight.BOLD,16));
 
         extract.setDisable(true);
         cancel.setDisable(true);
+        forgot.setDisable(true);
     }
 
 
